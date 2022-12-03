66570151e2c91bae3c1709b66de078a416570280
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-12-03 09:07:00 +0800

    Update links

--- pages/components/ProjectItem.jsx
+++ pages/components/ProjectItem.jsx
@@ -15,7 +15,7 @@ const ProjectItem = ({ title, backgroundImg, builtWith, projectUrl }) => {
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">{builtWith}</p>
-        <Link href={projectUrl} target="_blank" rel="noopener noreferrer">
+        <Link href={projectUrl ?? ""} target="_blank" rel="noopener noreferrer">
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More Info
           </p>
